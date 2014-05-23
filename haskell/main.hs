0282460f0dba9e09b325394ac0917fda8a282195
Shakeel Mohamed <smohamed@splunk.com>
2014-05-22 22:13:27 -0700

    Add gitignore and a Haskell attempt

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,7 +1,24 @@
-dateDiff s e = daysApart(s - e)
-daysApart t = floor (t / 24*60*60*1000)
--- Figure out how to declare arrays/lists
+module Main where
 
-main = 
-    --import split
-    splitOn "," (readFile "data.txt")
+import Data.List.Split (splitOn)
+
+dateDiff s e = daysApart(s - e)
+
+daysApart t = return floor (t / 24*60*60*1000)
+
+main = do
+    
+    --splitOn "," (readFile "data.txt")
+
+    -- This will print the whole file to the console
+    -- readFile "data.txt" >>= print
+
+    f <- readFile "data.txt"
+    let l = lines f 
+    
+    let todoTasks = lines f     
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+
+    -- print todoTasks
+    -- print numberedTasks
+    print map (splitOn "," show f)
